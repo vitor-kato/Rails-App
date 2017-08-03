a539817ef3d8157b8ea5b1f106beb1bea1564f95
vitor-kato <vitor.s.kato@gmail.com>
2017-08-03 17:35:13 +0000

    New controllers, links, and more

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,13 +1,39 @@
 class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+  
   def new
     @article = Article.new
   end
+  
   def create
     @article = Article.new(article_params)
-    @article.save
-    redirect_to articles_show(@article)
+    if @article.save
+      flash[:notice] = "Article saved"   
+      redirect_to article_path(@article)
+    else
+      render 'new'  
+    end  
   end
   
+  def show 
+    @article = Article.find(params[:id]) 
+  end
+  
+  def edit
+    @article = Article.find(params[:id])
+  end
+  
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article updated!"
+      redirect_to article_path(@article)
+    else
+      render :edit
+    end
+  end
   private
     def article_params
       params.require(:article).permit(:title, :description)
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ActiveRecord::Base
-  validates :title, presence: true
-  validates :description, presence: true
+  validates :title, presence: true, length: { minimum: 2 }
+  validates :description, presence: true, length: { minimum: 10 }
 end
